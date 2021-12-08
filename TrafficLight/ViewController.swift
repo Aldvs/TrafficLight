cb686440c5208081ed71da6134db77207236b282
aldvs <andrewgogol777@gmail.com>
2021-12-09 00:02:24 +0300

    setup main.storyboard and view controller with broken logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redView.alpha = 0.3
+        orangeView.alpha = 0.3
+        greenView.alpha = 0.3
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("START", for: .normal)
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = redView.layer.bounds.width / 2
+        orangeView.layer.cornerRadius = orangeView.layer.bounds.width / 2
+        greenView.layer.cornerRadius = greenView.layer.bounds.width / 2
+    }
 
-
+    @IBAction func startPressed() {
+        
+        if orangeView.alpha == 0.3 && greenView.alpha == 0.3 && redView.alpha == 0.3 {
+            startButton.setTitle("NEXT", for: .normal)
+            redView.alpha = 1
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.3
+            orangeView.alpha = 1
+        } else if orangeView.alpha == 1 {
+            orangeView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+    }
 }
 
